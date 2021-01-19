81e159e025dfee0263ee1b379fc93c41e7eb5994
Christos Traianos <xtraianos@gmail.com>
2021-01-19 12:52:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="icon" type="image/png" href="favicon-32x32.png" />
+	<title>IQOS</title>
+	<meta name="author" content=" " />
+	<meta name="description" content=" " />
+	<meta name="keywords"  content=" " />
+	<meta name="Resource-type" content="Document" />
+	<link rel="stylesheet" type="text/css" href="/css/fullpage.css" />
+	<link rel="stylesheet" type="text/css" href="/css/examples.css" />
+    <link rel="stylesheet" type="text/css" href="/css/style.css" />
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+</head>
+<body>
+
+
+
+<div class="loader">
+
+    <style>
+        .svg-loader-parent #line,
+        .svg-loader-parent #text,
+        .svg-loader-parent #inner-text{
+            opacity:0;
+        }
+    </style>
+
+    
+
+    <div class="svg-loader-parent">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 333.266 260.333">
+            <g id="Group_1157" data-name="Group 1157" transform="translate(0 0)">
+                <g id="text" data-name="Group 1152" transform="translate(143.271 0)">
+                    <g id="Group_1151" data-name="Group 1151">
+                        <path id="Path_726" data-name="Path 726" d="M1035.2,267.962c-10.605,0-21.439,1.89-21.439,15.89v8.508c0,14.01,10.834,15.9,21.439,15.9h9.273c10.611,0,21.451-1.89,21.451-15.9v-8.508c0-14-10.84-15.89-21.451-15.89Zm-14.627,24.025v-7.754c0-8.809,5.063-10.615,12.664-10.615h13.212c7.59,0,12.65,1.806,12.65,10.615v7.754c0,8.811-5.06,10.621-12.65,10.621h-13.212c-7.6,0-12.664-1.81-12.664-10.621" transform="translate(-933.642 -267.961)" fill="#fff"/>
+                        <path id="Path_727" data-name="Path 727" d="M962.041,267.961c-2.085,0-3.449,1.259-3.449,3.083v21.335c0,5.273,1.546,8.824,4.033,11.2,4.105,4.027,10.855,4.78,17.521,4.78h24.585l7.436,7.433a3.027,3.027,0,0,0,4.519-.189c1.486-1.489,1.489-3.271.35-4.414l-6.168-6.166V283.945c0-5.277-1.546-8.827-4.036-11.208-4.1-4.025-10.854-4.776-17.521-4.776Zm6.553,32.429c-.049-.038-.357-.3-.465-.4-1.558-1.52-2.541-3.886-2.6-7.548V273.734l27.339.016c3.607.129,6.217.811,8,2.181.048.04.36.305.468.4,1.555,1.519,2.537,3.886,2.6,7.544v18.69l-27.335.006c-3.614-.129-6.219-.812-8-2.184" transform="translate(-940.81 -267.961)" fill="#fff"/>
+                        <path id="Path_728" data-name="Path 728" d="M946.2,267.963h-.018a3.124,3.124,0,0,0-3.329,3.364v33.632a3.128,3.128,0,0,0,3.329,3.371l.018,0,.018,0a3.129,3.129,0,0,0,3.328-3.371V271.327a3.125,3.125,0,0,0-3.328-3.364H946.2" transform="translate(-942.855 -267.961)" fill="#fff"/>
+                        <path id="Path_729" data-name="Path 729" d="M1087.939,267.962c-15.777.078-18.974,3.883-18.974,9.983v2.282c0,7.563,3.52,10.5,12.529,10.5h19.747c7.937,0,8.554,2.331,8.554,5.627v.3c0,4.782-1.228,6-12.508,6,0,0-14.4-.009-14.734-.02v0c-3.752-.1-6.121-.539-7.22-2.495-.1-.207-.237-.5-.237-.5a3.287,3.287,0,0,0-3.322-2.088c-1.963,0-3.142,1.052-3.248,2.741a2.443,2.443,0,0,0,0,.561c.608,3.806,3.239,7.08,14.357,7.439v-.015c1.327.033,15.813.036,15.813.036,7.1,0,17.824,0,17.824-11.443v-.146c0-10.8-7.077-11.652-16.329-11.652h-16.9c-6.421,0-7.592-1.633-7.592-4.854v-1.45c0-4.29,1.852-5.159,11.01-5.159h11.169c5.414,0,8.668.123,10.016,2.532.105.207.24.5.24.5a3.285,3.285,0,0,0,3.319,2.088c1.963,0,3.143-1.052,3.251-2.745a2.574,2.574,0,0,0,0-.557c-.638-3.991-3.494-7.4-16.04-7.472Z" transform="translate(-926.527 -267.961)" fill="#fff"/>
+                    </g>
+                </g>
+                <path id="line" data-name="Path 730" d="M947.088,527.585l-128.173-69.5-2.856-137.71,71.208-46.955v39.237H881.2V284.688L822.194,323.6l2.714,130.836L946.9,520.581l121.787-74.765L1067.259,338.3l6.067-.081,1.474,110.967Z" transform="translate(-816.06 -267.252)" fill="#fff"/>
+                <g id="inner-text" data-name="Group 1154" transform="translate(52.655 67.76)">
+                    <g id="Group_1153" data-name="Group 1153">
+                        <path id="Path_731" data-name="Path 731" d="M864.2,327.929h3.576v8.906h9.714v-8.906h3.577v21.423h-3.577v-9.028h-9.714v9.028H864.2Z" transform="translate(-862.459 -327.929)" fill="#fff"/>
+                        <path id="Path_732" data-name="Path 732" d="M864.2,355.711H879.3v3.366H867.779v5.569h10.208v3.367H867.779v5.752h11.662v3.367H864.2Z" transform="translate(-862.459 -324.319)" fill="#fff"/>
+                        <path id="Path_733" data-name="Path 733" d="M880.986,355.711h16.548v21.423h-3.576V359.2h-9.395v17.934h-3.577Z" transform="translate(-860.278 -324.319)" fill="#fff"/>
+                        <path id="Path_734" data-name="Path 734" d="M899.911,355.711h3.577v21.423h-3.577Z" transform="translate(-857.818 -324.319)" fill="#fff"/>
+                        <path id="Path_735" data-name="Path 735" d="M906.638,355.711h15.849v3.336h-10.76l5.874,6.886-6.573,7.834h11.75v3.367H906.172v-2.785l6.95-8.2-6.485-7.59Z" transform="translate(-857.005 -324.319)" fill="#fff"/>
+                        <path id="Path_736" data-name="Path 736" d="M929.477,359.2h-6.456v-3.488H939.54V359.2h-6.456v17.934h-3.607Z" transform="translate(-854.815 -324.319)" fill="#fff"/>
+                        <path id="Path_737" data-name="Path 737" d="M940.387,355.711h3.576v8.905h9.714v-8.905h3.578v21.423h-3.578v-9.028h-9.714v9.028h-3.576Z" transform="translate(-852.559 -324.319)" fill="#fff"/>
+                        <path id="Path_738" data-name="Path 738" d="M959.515,355.711h3.81l6.194,10.13,6.194-10.13h3.81v21.423h-3.577V361.771l-6.427,10.1H969.4l-6.37-10.038v15.3h-3.518Z" transform="translate(-850.074 -324.319)" fill="#fff"/>
+                        <path id="Path_739" data-name="Path 739" d="M981.436,355.711h3.576v8.905h9.714v-8.905H998.3v21.423h-3.578v-9.028h-9.714v9.028h-3.576Z" transform="translate(-847.225 -324.319)" fill="#fff"/>
+                        <path id="Path_740" data-name="Path 740" d="M864.2,383.491h3.576v10.773l9.685-10.773h4.392l-8.463,9.212,8.842,12.21H877.9l-6.951-9.64-3.171,3.427v6.212H864.2Z" transform="translate(-862.459 -320.709)" fill="#fff"/>
+                        <path id="Path_741" data-name="Path 741" d="M890.816,383.356h3.315l8.957,21.575h-3.781l-2.064-5.172h-9.627l-2.093,5.172h-3.664Zm5.119,13.068-3.519-8.569-3.491,8.569Z" transform="translate(-860.164 -320.727)" fill="#fff"/>
+                        <path id="Path_742" data-name="Path 742" d="M903.282,383.491H906.6L917.5,398.3V383.491h3.52v21.423h-3L906.8,389.674v15.24h-3.518Z" transform="translate(-857.38 -320.709)" fill="#fff"/>
+                        <path id="Path_743" data-name="Path 743" d="M923.182,383.491h15.094v3.367H926.759v5.569h10.208v3.367H926.759v5.753h11.662v3.366h-15.24Z" transform="translate(-854.795 -320.709)" fill="#fff"/>
+                        <path id="Path_744" data-name="Path 744" d="M940.089,383.491h3.577v21.423h-3.577Z" transform="translate(-852.598 -320.709)" fill="#fff"/>
+                        <path id="Path_745" data-name="Path 745" d="M960.441,386.98h-6.457v-3.489H970.5v3.489h-6.457v17.933h-3.607Z" transform="translate(-850.792 -320.709)" fill="#fff"/>
+                        <path id="Path_746" data-name="Path 746" d="M971.35,383.491h3.576V392.4h9.714v-8.906h3.578v21.423h-3.578v-9.027h-9.714v9.027H971.35Z" transform="translate(-848.536 -320.709)" fill="#fff"/>
+                        <path id="Path_747" data-name="Path 747" d="M871.412,411.137h3.316l8.754,21.576H862.659Zm1.6,4.621-5.351,13.588h10.7Z" transform="translate(-862.659 -317.117)" fill="#fff"/>
+                        <path id="Path_748" data-name="Path 748" d="M883.809,411.273h3.577V432.7h-3.577Z" transform="translate(-859.911 -317.099)" fill="#fff"/>
+                        <path id="Path_749" data-name="Path 749" d="M898.6,411.137h3.315l8.958,21.576h-3.781l-2.066-5.172H895.4l-2.093,5.172h-3.664Zm5.119,13.068-3.52-8.569-3.489,8.569Z" transform="translate(-859.152 -317.117)" fill="#fff"/>
+                        <path id="Path_750" data-name="Path 750" d="M932.236,421.917c0,5.571-4.218,8.691-9.889,9.028v1.99h-3.432v-1.99c-5.7-.306-9.859-3.366-9.859-8.936v-.062c0-5.539,4.188-8.905,9.859-9.242v-1.623h3.432v1.623C928.047,412.981,932.236,416.347,932.236,421.917ZM918.915,415.8c-3.869.245-6.253,2.724-6.253,6.091v.061c0,3.366,2.415,5.6,6.253,5.877Zm3.432,12.028c3.84-.215,6.253-2.45,6.253-5.815v-.062c0-3.366-2.414-5.876-6.253-6.121Z" transform="translate(-856.63 -317.124)" fill="#fff"/>
+                        <path id="Path_751" data-name="Path 751" d="M932.117,422.088v-.061c0-6.029,4.42-11.079,10.672-11.079s10.616,4.988,10.616,11.017c0,6.06-4.391,11.14-10.673,11.14C936.479,433.105,932.117,428.116,932.117,422.088Zm17.537,0v-.061c0-4.163-2.879-7.591-6.922-7.591s-6.863,3.367-6.863,7.529v.062c0,4.162,2.879,7.619,6.921,7.619S949.654,426.25,949.654,422.088Z" transform="translate(-853.634 -317.141)" fill="#fff"/>
+                        <path id="Path_752" data-name="Path 752" d="M954.25,411.273h8.026c4.741,0,7.737,2.846,7.737,7.162v.061c0,4.8-3.665,7.314-8.143,7.314h-4.043V432.7H954.25Zm7.736,11.14c2.7,0,4.392-1.592,4.392-3.826v-.061c0-2.51-1.716-3.826-4.392-3.826h-4.16v7.713Z" transform="translate(-850.758 -317.099)" fill="#fff"/>
+                        <path id="Path_753" data-name="Path 753" d="M976.828,411.137h3.315l8.957,21.576h-3.781l-2.064-5.172h-9.627l-2.092,5.172h-3.665Zm5.118,13.068-3.52-8.569-3.489,8.569Z" transform="translate(-848.988 -317.117)" fill="#fff"/>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    </div>
+    <div class="loading-bar"></div>
+
+    <script>
+        gsap.to('.svg-loader-parent #text', {
+            duration: 0.5,
+            opacity: 1
+        })
+        gsap.from('.svg-loader-parent #text', {
+            delay: 2,
+            duration: 1.9,
+            ease: 'power3.inOut',
+            y: 100,
+            x: 38,
+            scale: 1.5
+        })
+        gsap.to('.svg-loader-parent #line, .svg-loader-parent #inner-text', {
+            duration: 1.2,
+            opacity: 1,
+            delay: 2.9
+        })
+        gsap.to('.loading-bar', {
+            duration: 12,
+            scaleX: 1,
+            delay: 1,
+            ease: 'power2.inOut'
+        })
+
+        gsap.to('.loading-bar', {
+            opacity: 0,
+            duration: 0.5,
+            delay: 13,
+        })
+        gsap.to('.loader', {
+            y: '-100%',
+            duration: 2,
+            ease: 'power3.inOut',
+            delay: 13,
+        })
+        gsap.to('.svg-loader-parent', {
+            y: 700,
+            duration: 2,
+            ease: 'power3.inOut',
+            delay: 13,
+        })
+        gsap.to('.svg-loader-parent', {
+            opacity: 0,
+            duration: 1.7,
+            ease: 'power3.inOut',
+            delay: 13,
+        })
+
+    </script>
+
+    
+</div>
+
+
+
+<?php include 'header.php';?>
+    
+<div id="fullpage">
+    <div class="section active" id="sectionHome">
+        <div class="container">
+            <div class="line">
+                <div class="home-button">
+                    <a href="#" data-modal="epistimi"  class="home-icon">
+                        <img src="assets/icons/01.svg">
+                    </a>
+                    <h3>Επιστήμη</h3>
+                </div>
+                
+                <div class="home-button">
+                    <a href="#" data-modal="texnologia-aixmis" class="home-icon">
+                        <img src="assets/icons/02.svg">
+                    </a>
+                    <h3>Τεχνολογία αιχμής</h3>
+                </div>
+                
+                <div class="home-button">
+                    <a href="#" data-modal="kapnos-vs-aerolyma" class="home-icon">
+                        <img src="assets/icons/03.svg">
+                    </a>
+                    <h3>Καπνός vs Αερόλυμα</h3>
+                </div>
+                
+                <div class="home-button">
+                    <a href="#" data-modal="ravdos-kapnou" class="home-icon">
+                        <img src="assets/icons/04.svg">
+                    </a>
+                    <h3>Ράβδος καπνού <br>HEETS</h3>
+                </div>
+            </div>
+            
+            <div class="line">
+                <div class="home-button">
+                    <a href="#" data-modal="poiotita-kapnou" class="home-icon">
+                        <img src="assets/icons/05.svg">
+                    </a>
+                    <h3>Ποιότητα καπνού</h3>
+                </div>
+                
+                <div class="home-button">
+                    <a href="#" data-modal="nikotini" class="home-icon">
+                        <img src="assets/icons/06.svg">
+                    </a>
+                    <h3>Νικοτίνη</h3>
+                </div>
+                
+                <div class="home-button">
+                    <a href="#" data-modal="thermansi-vs-ilektroniko" class="home-icon">
+                        <img src="assets/icons/07.svg">
+                    </a>
+                    <h3>Θέρμανση καπνού vs<br>Ηλεκτρονικό τσιγάρο</h3>
+                </div>
+                
+                <div class="home-button">
+                    <a href="#" data-modal="faq" class="home-icon">
+                        <img src="assets/icons/08.svg">
+                    </a>
+                    <h3>Συχνές ερωτήσεις</h3>
+                </div>
+            </div>
+            
+        </div>                 
+    </div>	
+</div>
+
+<div class="modal" data-modal="epistimi">
+    <?php include 'epistimi.php';?>
+</div>
+
+<div class="modal" data-modal="texnologia-aixmis">
+    <?php include 'texnologia-aixmis.php';?>
+</div>
+
+<div class="modal" data-modal="kapnos-vs-aerolyma">
+    <?php include 'kapnos-vs-aerolyma.php';?>
+</div>
+
+<div class="modal" data-modal="ravdos-kapnou">
+    <?php include 'ravdos-kapnou.php';?>
+</div>
+
+<div class="modal" data-modal="nikotini">
+    <?php include 'nikotini.php';?>
+</div>
+
+<div class="modal" data-modal="poiotita-kapnou">
+    <?php include 'poiotita-kapnou.php';?>
+</div>
+
+<div class="modal" data-modal="thermansi-vs-ilektroniko">
+    <?php include 'thermansi-vs-ilektroniko.php';?>
+</div>
+
+<div class="modal" data-modal="faq">
+    <?php include 'faq.php';?>
+</div>
+
+
+
+
+<script src="/scripts/jquery.min.js"></script>
+<!-- <script src="/scripts/jquery-ui.min.js"></script> -->
+<script type="text/javascript" src="/scripts/fullpage.js"></script>
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.5.9/lottie.min.js"></script>
+<script src="scripts/main.js"></script>
+
+</body>
+</html>
